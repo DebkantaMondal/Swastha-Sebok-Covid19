16548f4756f6d39e911992d4b351b1635d47379d
DebkantaMondal <debkantaroni01@gmail.com>
2020-05-14 14:41:11 +0530

    Updates

---
+++ precache-manifest.5a7425b66e5a4063db1ccc9ad625bc46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc927cd481488c54c026773869ffe52",
+    "url": "/Swastha-Sebok-Covid19/index.html"
+  },
+  {
+    "revision": "11511d2a20841aed11b3",
+    "url": "/Swastha-Sebok-Covid19/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "5a3d517da916a2395f3e",
+    "url": "/Swastha-Sebok-Covid19/static/js/2.3a95ceaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Swastha-Sebok-Covid19/static/js/2.3a95ceaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "11511d2a20841aed11b3",
+    "url": "/Swastha-Sebok-Covid19/static/js/main.4abe46a6.chunk.js"
+  },
+  {
+    "revision": "82254217d71ff3b3e46b",
+    "url": "/Swastha-Sebok-Covid19/static/js/runtime-main.ddf64bbb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Swastha-Sebok-Covid19/static/media/logo.5d5d9eef.svg"
+  }
+]);
